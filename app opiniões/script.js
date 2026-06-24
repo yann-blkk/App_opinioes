741216122e9af414d64dfa0ed9a882d13fea09e7
yann-blkk <yan.lucas.yjj@gmail.com>
2026-06-24 16:53:14 -0300

    Add files via upload

--- app opiniões/script.js
+++ app opiniões/script.js
@@ -1,10 +1,15 @@
 function adicionarAvaliacao() {
-    const nome = document.getElementById("appNome").value;
-    const nota = document.getElementById("nota").value;
-    const comentario = document.getElementById("comentario").value;
+    const nome = document.getElementById("appNome").value.trim();
+    const nota = parseFloat(document.getElementById("nota").value);
+    const comentario = document.getElementById("comentario").value.trim();
 
-    if (nome === "" || nota === "" || comentario === "") {
-        alert("Preencha todos os campos!");
+    if (nome === "" || isNaN(nota) || comentario === "") {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+    if (nota < 0 || nota > 5) {
+        alert("A nota deve ser entre 0 e 5");
         return;
     }
 
@@ -21,8 +26,21 @@ function adicionarAvaliacao() {
 
     lista.appendChild(div);
 
-    
+    // Atualiza médias (se for app conhecido)
+    if (dados[nome]) {
+        dados[nome].push(nota);
+        atualizarMedias();
+    }
+
+    // limpar campos
     document.getElementById("appNome").value = "";
     document.getElementById("nota").value = "";
     document.getElementById("comentario").value = "";
+}
+
+function mostrarAba(aba) {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("form").style.display = "none";
+
+    document.getElementById(aba).style.display = "block";
 }
